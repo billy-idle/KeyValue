c673ba3252ee98f882d273b82adc0bbffec44586
gguzmans <gguzmans@emeal.nttdata.com>
2022-12-12 09:39:45 -0500

    Initial commit

---
+++ src/test/java/com/github/billy/keyvalue/KeyValueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.billy.keyvalue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyValueApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
