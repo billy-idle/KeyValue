b2d56999484c0fd4df383462f8426827771797b0
gguzmans <gguzmans@emeal.nttdata.com>
2022-12-12 11:45:53 -0500

    Iteration complement

--- src/main/java/com/github/billy/keyvalue/KeyValueApplication.java
+++ src/main/java/com/github/billy/keyvalue/KeyValueApplication.java
@@ -1,8 +1,18 @@
 package com.github.billy.keyvalue;
 
+import com.github.billy.keyvalue.repository.Parameters;
+import com.github.billy.keyvalue.repository.ParametersRepository;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+@Slf4j
 @SpringBootApplication
 public class KeyValueApplication {
 
@@ -10,4 +20,26 @@ public class KeyValueApplication {
     SpringApplication.run(KeyValueApplication.class, args);
   }
 
+  @Bean
+  CommandLineRunner runner(ParametersRepository repository) {
+    return args -> {
+      Parameters parameters = new Parameters();
+      Set<String> values = new HashSet<>();
+      values.add("gguzman@emeal.nttdata.com");
+      values.add("oca2020@protonmail.com");
+      values.add("oca2020@protonmail.com");
+      parameters.setKey("addresses");
+      parameters.setValues(values);
+      repository.save(parameters);
+
+      List<Parameters> parametersFound = new ArrayList<>();
+      parametersFound = repository.findAll();
+      for (Parameters p : parametersFound) {
+        log.info("Parameter: {}", p);
+/*        for (String s : p.getValues()) {
+          log.info("Value: {}", s);
+        }*/
+      }
+    };
+  }
 }
--- src/main/java/com/github/billy/keyvalue/repository/ParametersRepository.java
+++ src/main/java/com/github/billy/keyvalue/repository/ParametersRepository.java
@@ -1,2 +1,9 @@
-package com.github.billy.keyvalue.repository;public interface ParametersRepository {
+package com.github.billy.keyvalue.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ParametersRepository extends JpaRepository<Parameters, Long> {
+
 }
